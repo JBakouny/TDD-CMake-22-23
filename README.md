0c1fbb8dce4672d57547e388d1bba3fd7c04f92b
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2022-11-09 11:39:32 +0200

    Simple CMake Project

---
+++ README.md
@@ -0,0 +1,10 @@
+# CMake Commands
+
+## Configure when you change CMakeLists.txt:
+`
+cmake -S . -B build
+`
+## Compile whe=n you change code:
+`
+cmake --build build
+`
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+// #include "Matrix/Matrix.hpp"
+using namespace std;
+
+
+int main() {
+
+    // Matrix m({10, 20, 
+    //         30, 40});
+    
+    // cout << m(0,0) << endl;
+
+    cout << "Hello World" << endl;
+    return 0;
+}
